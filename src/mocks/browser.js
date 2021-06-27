d4253cf8bd16305ce82b41a490b402baef9fdf02
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-06-27 11:27:00 -0400

    set up code grade

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,4 +1,4 @@
 import { setupWorker } from 'msw'
 import { handlers } from './handlers'
-// This configures a Service Worker with the given request handlers.
+// This configures a Service Worker with the giv en request handlers.
 export const worker = setupWorker(...handlers)
